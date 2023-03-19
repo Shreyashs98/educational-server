07e53e9b2f81cd573bd01419660f6e7cdca59720
Shreyashs98 <104050227+Shreyashs98@users.noreply.github.com>
2023-03-19 09:40:45 +0530

    update 19/3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-require( './db/init.js' ); // the file executes and connects to the DB
+require( './db/init.js' ); 
 
 const express = require( 'express' );
 const materialsRouter = require( './routes/materials.route' );
 const cors = require( 'cors' );
 
 const app = express();
-
-// middleware (router is also a middleware) - function that gets called on each request
-// express.json() is a middleware which will read the request body, and if it finds JSON data, it will set that data on req.body
-// should be before all other middleware
 app.use( cors() );
 app.use( express.json() );
 app.use( materialsRouter );
